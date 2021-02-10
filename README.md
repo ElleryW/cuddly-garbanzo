1776da93dfc81938d5a024caaaef70f64236d03a
Ellery Womack <ellery.womack@gmail.com>
2021-02-09 21:53:51 -0600

    it works for basic requirements

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 crawl a site, find words, impress your friends
 
 simply run `node index.js <site> <searchTerm>`
+don't forget `npm install` or `npm ci`
--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const axios = require("axios");
 const args = process.argv.slice(2);
 // FIXME - kill hard coded values
 const host = args[0] || "https://www.apple.com";
-const searchTerm = args[1] || "apple";
-const parsedLinks = [];
-let pagesCrawled = 0;
+const searchTerm = args[1] || "card";
+const crawledLinks = [];
+const termLinks = [];
+const depth = 2;
 
+// Get some inputs
 if (!host || !searchTerm) {
   console.error("Missing argument");
   process.exit(1);
@@ -15,25 +17,61 @@ if (!host || !searchTerm) {
 
 const getPage = async (path) => {
   // Don't parse a link we already parsed as a descendent of another page
-  if (parsedLinks.includes(path)) return null;
+  if (crawledLinks.includes(path)) return null;
   // Track link
-  parsedLinks.push(path);
-  pagesCrawled++;
+  crawledLinks.push(path);
 
-  const response = await axios.get(`${host}/${path}`);
-  return response.data;
+  try {
+    console.log(`Crawling page: ${host}${path}`);
+    const response = await axios.get(`${host}${path}`);
+    return response.data;
+  } catch (error) {
+    //console.error(error);
+  }
 };
 
-getPage("/").then((data) => {
-  const soup = new JSSoup(data);
+const findSearchTerm = (soup, path) => {
   const text = soup.text;
-  if (text.indexOf(searchTerm) > -1) console.log(text);
+  if (text.indexOf(searchTerm) > -1) {
+    termLinks.push(path);
+  }
+};
+
+const parseLinks = (soup) => {
   const links = soup.findAll("a").map((x) => x.attrs.href);
   const filteredLinks = [];
   links.forEach((x) => {
-    // Ditch anchor links and remove duplicates
-    if (x.indexOf("#") === -1 && !filteredLinks.includes(x))
+    if (
+      x &&
+      // Ditch anchor links
+      x.indexOf("#") === -1 &&
+      // skip anything with a full path as we only want relative links
+      x.indexOf("://") === -1 &&
+      // Remove duplicates
+      !filteredLinks.includes(x)
+    )
       filteredLinks.push(x);
   });
-  console.log(filteredLinks);
+  return filteredLinks;
+};
+
+const search = async (path, currentDepth) => {
+  if (currentDepth >= depth) return;
+
+  const html = await getPage(path); //.then((html) => {
+  if (!html) return;
+  const soup = new JSSoup(html);
+  findSearchTerm(soup, path);
+  const links = parseLinks(soup);
+  for (const link of links) {
+    await search(link, currentDepth + 1);
+  }
+};
+
+// Start with root path
+search("/", 0).then((data) => {
+  console.log(
+    `Crawled ${crawledLinks.length} pages and found search term '${termLinks.length}' times on the following pages:`
+  );
+  console.log(termLinks);
 });
